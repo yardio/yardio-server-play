48d338867dd8fb801d10c22ce85dc618ea6b3d88
Paul Dijou <paul.dijou@gmail.com>
2014-07-19 16:42:06 +1200

    Temporary display full config

--- app/Global.scala
+++ app/Global.scala
@@ -1,3 +1,4 @@
+import play.api.Play
 import play.api._
 import play.api.mvc._
 
@@ -5,6 +6,11 @@ import controllers._
 
 object Global extends GlobalSettings {
   def default(req: RequestHeader): Handler = super.onRouteRequest(req) getOrElse Action { Results.NotFound }
+
+  override def onStart(app: Application) {
+    Logger.info("Application has started")
+    Logger.info(Play.current.configuration.underlying.root().render())
+  }
 
   override def onRouteRequest(req: RequestHeader): Option[Handler] = (req.method, req.path) match {
     case ("GET", "/debug") => Option(Debug.display)
